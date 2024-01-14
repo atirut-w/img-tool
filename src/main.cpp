6710593ffe91c11f982fefa1a1adba83dd97e834
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-01-14 15:03:05 +0700

    Attempt to open archive file

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,37 @@
-int main()
+#include <argparse/argparse.hpp>
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char *argv[])
 {
+    argparse::ArgumentParser program("imgtool");
+    
+    program.add_argument("archive")
+        .help("The archive to operate on");
+    
+    auto &actions = program.add_mutually_exclusive_group();
+
+    actions.add_argument("-l", "--list")
+        .help("List the contents of the archive")
+        .flag();
+    
+    try
+    {
+        program.parse_args(argc, argv);
+    }
+    catch (const std::runtime_error &err)
+    {
+        std::cout << err.what() << std::endl;
+        std::cout << program;
+        exit(0);
+    }
+
+    std::ifstream file(program.get<std::string>("archive"), std::ios::binary);
+    if (!file)
+    {
+        std::cout << "Failed to open archive" << std::endl;
+        return 1;
+    }
+    
     return 0;
 }
